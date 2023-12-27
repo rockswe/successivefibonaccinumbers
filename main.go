4ba29d28c724d75c8231818bdb52724a0fabcef6
Efe Kaya <115823106+rockswe@users.noreply.github.com>
2023-12-26 23:53:47 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// fibonacci returns a function that returns successive fibonacci numbers.
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		ret := a
+		a, b = b, a+b
+		return ret
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
